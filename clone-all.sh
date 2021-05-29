b26fd05c1ae95efefa4787aa271b5dd254b4137a
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2021-05-29 12:54:58 +0200

    Script Updated ✏

--- clone-all.sh
+++ clone-all.sh
@@ -4,53 +4,64 @@ echo '------------------------------------------------------'
 git clone https://github.com/GabrielCrackPro/vscode-settings.git #VS Code settings
 echo '------------------------------------------------------'
 
-echo 'Cloned 1/18'
+echo 'Cloned 1/20'
 git clone https://github.com/GabrielCrackPro/Setup.git #Setup
-echo 'Cloned 2/18'
+echo 'Cloned 2/20'
 echo '------------------------------------------------------'
 git clone https://github.com/GabrielCrackPro/Words-API-App.git #Words API App
-echo 'Cloned 3/18'
+echo 'Cloned 3/20'
 echo '------------------------------------------------------'
 git clone https://github.com/GabrielCrackPro/GabrielCrackPro.git #Profile ReadMe
-echo 'Cloned 4/18'
+echo 'Cloned 4/20'
 echo '------------------------------------------------------'
 git clone https://github.com/GabrielCrackPro/pokemon-tcg-app.git #Pokemon TCG App
-echo 'Cloned 5/18'
+echo 'Cloned 5/20'
 echo '------------------------------------------------------'
 git clone https://github.com/GabrielCrackPro/TODO-App.git #TODO App
-echo 'Cloned 6/18'
+echo 'Cloned 6/20'
 echo '------------------------------------------------------'
 git clone https://github.com/GabrielCrackPro/Google-Simulator.git #Google Clone
-echo 'Cloned 7/18'
+echo 'Cloned 7/20'
 echo '------------------------------------------------------'
 git clone https://github.com/GabrielCrackPro/Portfolio.git #Portfolio
-echo 'Cloned 8/18'
+echo 'Cloned 8/20'
 echo '------------------------------------------------------'
 git clone https://github.com/GabrielCrackPro/duckduckgo-Simulator.git #Duckduckgo Clone
-echo 'Cloned 9/18'
+echo 'Cloned 9/20'
 echo '------------------------------------------------------'
 git clone https://github.com/GabrielCrackPro/CPP-Projects.git #C++ Projects
-echo 'Cloned 10/18'
+echo 'Cloned 10/20'
 echo '------------------------------------------------------'
 git clone https://github.com/GabrielCrackPro/Contacts-App-v2.0.git #Contacts App
-echo 'Cloned 11/18'
+echo 'Cloned 11/20'
 echo '------------------------------------------------------'
 git clone https://github.com/GabrielCrackPro/Python-Scripts.git #Python Scripts
-echo 'Cloned 12/18'
+echo 'Cloned 12/20'
 echo '------------------------------------------------------'
 git clone https://github.com/GabrielCrackPro/JS-Calculator-v2.git # JS Calculator
-echo 'Cloned 13/18'
+echo 'Cloned 13/20'
 echo '------------------------------------------------------'
 git clone https://github.com/GabrielCrackPro/Periodic-Table.git #Periodic Table
-echo 'Cloned 14/18'
+echo 'Cloned 14/20'
 echo '------------------------------------------------------'
 git clone https://github.com/GabrielCrackPro/RealTime-Clock.git #RealTime Clock
-echo 'Cloned 15/18'
+echo 'Cloned 15/20'
 echo '------------------------------------------------------'
 git clone https://github.com/GabrielCrackPro/JS-Reddit-Clone.git #Reddit Clone
-echo 'Cloned 16/18'
+echo 'Cloned 16/20'
 echo '------------------------------------------------------'
 git clone https://github.com/GabrielCrackPro/JS-TaskApp.git #JS Task App
-echo 'Cloned 17/18'
+echo 'Cloned 17/20'
 echo '------------------------------------------------------'
 git clone https://github.com/GabrielCrackPro/beer-counter.git #Beer Counter
+echo 'Cloned 18/20'
+echo '------------------------------------------------------'
+git clone https://github.com/GabrielCrackPro/quotes-app.git #Quotes App
+echo 'Cloned 19/20'
+echo '------------------------------------------------------'
+git clone https://github.com/GabrielCrackPro/trivia-app.git #Trivia App
+echo 'Cloned 20/20'
+echo '------------------------------------------------------'
+git clone https://github.com/GabrielCrackPro/cors-fix-localhost.git #CORS fix on localhost
+echo '------------------------------------------------------'
+echo 'Done'
